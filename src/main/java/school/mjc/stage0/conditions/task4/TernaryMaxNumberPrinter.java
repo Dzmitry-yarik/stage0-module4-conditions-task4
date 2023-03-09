913c028af53eba485727099dd1d2e5f346257649
Dzmitry-yarik <107866389+Dzmitry-yarik@users.noreply.github.com>
2023-03-09 15:59:02 +0300

    Update TernaryMaxNumberPrinter.java

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
@@ -2,12 +2,8 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryMaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
-           int a = first > second ? first: second;
+       int a = first > second ? first: second;
         a = a > third ? a: third;
-        if (a == first)
-        System.out.println( "first. Greatest from 3");
-        else if (a == second) {
-            System.out.println( "second. Greatest from 3");
-        } else System.out.println( "third. Greatest from 3");
+        System.out.println(a);
     }
 }
